d364e6bf92bb22084e02b9ce907b0d7022b74fa0
saileshgg <saileshgurung36@gmail.com>
2022-01-14 17:24:59 +0530

    upto problem 1

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+const firstAngle = document.querySelector("#first-angle");
+const secondAngle = document.querySelector("#second-angle");
+const thirdAngle = document.querySelector("#third-angle");
+const checkNumber = document.querySelector("#check-number");
+const output = document.querySelector("#output");
+
+
+checkNumber.addEventListener("click", function(){
+    isTriangle(firstAngle.value, secondAngle.value, thirdAngle.value);
+ 
+})
+
+function isTriangle(n1,n2,n3) {
+    var sum = parseInt(n1) + parseInt(n2) + parseInt(n3);
+    if(sum === 180) {
+        output.innerText = "yes";
+    } else {
+        output.innerText = "no";
+    }
+
+
+    // output.innerHTML = sum;
+}
