b8492dd7d63e061f71c2a9453679265b999d86bc
saileshgg <saileshgurung36@gmail.com>
2022-01-14 20:52:09 +0530

    structure done

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-const firstAngle = document.querySelector("#first-angle");
-const secondAngle = document.querySelector("#second-angle");
-const thirdAngle = document.querySelector("#third-angle");
+// const firstAngle = document.querySelector("#first-angle");
+// const secondAngle = document.querySelector("#second-angle");
+// const thirdAngle = document.querySelector("#third-angle");
+
+const inputAngle = document.querySelectorAll(".angleInput");
 const checkNumber = document.querySelector("#check-number");
 const output = document.querySelector("#output");
 
 
 checkNumber.addEventListener("click", function(){
-    isTriangle(firstAngle.value, secondAngle.value, thirdAngle.value);
- 
+
+    if(inputAngle[0].value && inputAngle[1].value && inputAngle[2].value)
+        isTriangle(inputAngle[0].value, inputAngle[1].value, inputAngle[2].value);
+    else 
+        output.innerText = "Enter all the values";
 })
 
 function isTriangle(n1,n2,n3) {
     var sum = parseInt(n1) + parseInt(n2) + parseInt(n3);
     if(sum === 180) {
-        output.innerText = "yes";
+        output.innerText = "YES";
     } else {
-        output.innerText = "no";
+        output.innerText = "NO";
     }
 
 
